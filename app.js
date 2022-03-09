d450f4e34da9d522d6287a95f98bc1d2350c216a
Lauro Neto <lauro.neto@01x.com.br>
2022-03-09 15:21:49 -0300

    ultima att

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ global.url = path.resolve(path.join(__dirname, 'public'));
 
 venom
 .create({
-    session: '01x', //name of session
+    session: 'sice', //name of session
     multidevice: true // for version not multidevice use false.(default: true)
 })
 .then((client) => start(client))
--- auth/token.js
+++ auth/token.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 
 function verifyJWT(req, res, next){
     const token = req.headers['x-access-token'];
-    console.log(token , 's');
     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
     
     jwt.verify(token, process.env.SECRET, function(err, decoded) {
